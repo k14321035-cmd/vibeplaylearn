e6da16990af34d179253df1588b528de2af7a4d3
k14321035-cmd <k14321035@gmail.com>
2026-02-08 20:31:11 +0530

    Delete server directory

--- server/src/server.js
+++
@@ -1,8 +0,0 @@
-import http from "http";
-import app from "./app.js";
-
-const server = http.createServer(app);
-
-server.listen(3000, () => {
-  console.log("🚀 Server running on http://localhost:3000");
-});
