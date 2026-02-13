5d29d57f362d31acf8f0692b9db4f22d2a4cb987
k14321035-cmd <k14321035@gmail.com>
2026-02-13 19:05:15 +0530

    Add files via upload

--- backend/src/routes/story.routes.js
+++ backend/src/routes/story.routes.js
@@ -3,8 +3,6 @@ import { uploadStory, getFeedStories } from "../controllers/story.controller.js"
 import { protect } from "../middleware/auth.middleware.js"; // Use your existing auth
 import upload from "../middleware/upload.js"; // Use the multer config we created
 const router = express.Router();
-
-router.post("/", protect, uploadStory);
 router.get("/feed", protect, getFeedStories);
 router.post("/", protect, upload.single("image"), uploadStory);
 
